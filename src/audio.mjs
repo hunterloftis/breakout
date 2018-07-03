5f4641a8a35c928eac6b2c7158b2c555bec43d00
Hunter Loftis <hunter@hunterloftis.com>
2018-07-03 16:15:36 -0400

    volume control

--- src/audio.mjs
+++ src/audio.mjs
@@ -2,29 +2,30 @@ export default class Audio {
   constructor() {
     document.addEventListener('click', () => {
       this.ctx = new window.AudioContext()
+      this.gain = this.ctx.createGain()
+      this.gain.gain.value = 0.3
+      this.gain.connect(this.ctx.destination)
     })
   }
   play(events, delta, time) {
     if (!this.ctx) return
-    if (events.smash) smash(this.ctx)
-    if (events.bounce) bounce(this.ctx)
+    if (events.smash) this.smash()
+    if (events.bounce) this.bounce()
   }
-}
-
-function bounce(ctx) {
-  const b = ctx.createOscillator()
-  b.type = 'sine'
-  b.frequency.value = 2092
-  b.connect(ctx.destination)
-  b.start(ctx.currentTime)
-  b.stop(ctx.currentTime + 0.05)
-}
-
-function smash(ctx) {
-  const s = ctx.createOscillator()
-  s.type = 'sawtooth'
-  s.frequency.value = 4194
-  s.connect(ctx.destination)
-  s.start(ctx.currentTime)
-  s.stop(ctx.currentTime + 0.05)
+  bounce() {
+    const b = this.ctx.createOscillator()
+    b.type = 'sine'
+    b.frequency.value = 2092
+    b.connect(this.gain)
+    b.start(this.ctx.currentTime)
+    b.stop(this.ctx.currentTime + 0.05)
+  }
+  smash() {
+    const s = this.ctx.createOscillator()
+    s.type = 'sawtooth'
+    s.frequency.value = 4194
+    s.connect(this.gain)
+    s.start(this.ctx.currentTime)
+    s.stop(this.ctx.currentTime + 0.05)
+  }
 }
